17ea3f874964e3228a0a65f37ce27f1bacb5b6f6
icegriffinguru <icegriffin1122@gmail.com>
2022-01-20 09:38:09 -0800

    0.0.2

--- test/test1.js
+++ test/test1.js
@@ -2,9 +2,18 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("NODERewardManagement", function () {
-  it("Should be deployed", async function () {
+  let iterableMapping;
+  let nodeRewardManager;
+  let owner, addr1, addr2, addrs;
+
+  // `beforeEach` will run before each test, re-deploying the contract every
+  // time. It receives a callback, which can be async.
+  beforeEach(async function () {
+    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+
+    // Get the ContractFactory and Signers here.
     const IterableMapping = await ethers.getContractFactory("IterableMapping");
-    const iterableMapping = await IterableMapping.deploy();
+    iterableMapping = await IterableMapping.deploy();
 
     const NODERewardManagement = await ethers.getContractFactory("NODERewardManagement", {
       libraries: {
@@ -15,10 +24,21 @@ describe("NODERewardManagement", function () {
     const _nodePrice = 50;
     const _rewardPerNode = 10;
     const _claimTime = 100;
-    const nodeRewardManager = await NODERewardManagement.deploy(_nodePrice, _rewardPerNode, _claimTime);
+    nodeRewardManager = await NODERewardManagement.deploy(_nodePrice, _rewardPerNode, _claimTime);
     await nodeRewardManager.deployed();
 
     // check address is not zero
     expect(await nodeRewardManager.address).to.not.equal(0);
+  });
+
+  it("Should be deployed", async function () {
+    //address account, string memory nodeName
+    const nodeName1 = 'Account1 - Node1';
+    nodeRewardManager.createNode(addr1.address, nodeName1);
+
+    // nodeRewardManager.createNode(addr1.address, nodeName1);
+    // expect(() => {
+    //   nodeRewardManager.createNode(addr1.address, nodeName1);
+    // }).to.throw('Cannot create a node with the same name by one user');
   });
 });
